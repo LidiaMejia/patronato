42416d6158479a05ed028b1ce47b9ccb76d17d89
Lidia Mejia <lidiamejia29@yahoo.com>
2020-07-05 02:12:04 -0600

    Creación del Middleware
    Creación del Middleware para gestionar acceso a rutas privadas a través del passport y el JWT en el index.js de api (El superconmutador).

--- bckpatronato/routes/api/index.js
+++ bckpatronato/routes/api/index.js
@@ -4,6 +4,28 @@
 
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var passportJWT = require('passport-jwt');
+
+var strategyJWT = passportJWT.Strategy;  //Clase para Definir la Estrategia
+var extractJWT = passportJWT.ExtractJwt; //Clase para Extraer el JWT
+
+
+//Usar una nueva estrategia de Autenticación con JWS
+passport.use(
+    new strategyJWT(
+        {
+            jwtFromRequest: extractJWT.fromAuthHeaderAsBearerToken(), //De la solicitud extraigo el JWT como Bearer Token
+            secretOrKey: process.env.JWT_SECRET                       //Y se debe desencriptar con la cadena de .env
+        },
+        //El segundo parámetro es una función anónima donde están todos los datos guardados del Login y el Next para seguir con la sig ruta
+        (payload, next)=>{
+            var user = payload; //Tomar datos del Login
+            return next(null, user); //Retornar a la siguiente ruta con los datos del usuario
+        }
+    )
+)
+
 
 
 //SE UNE CADA ENTIDAD EN UN SOLO ELEMENTO PARA EXPONERLO
@@ -20,9 +42,17 @@ var alumnosRoutes = require('./alumnos');
 //2. Exponer las Rutas
 //Se indica: La carpeta donde estan esas rutas y las rutas que se van a exponer (Guardadas anteriormente)
 //Se usa todo lo que esta en la carpeta que ya viene en la variable
+
+//Rutas Públicas
 router.use('/sec', secRoutes);
-router.use('/mocion', mocionRoutes);
-router.use('/alumnos', alumnosRoutes); 
+
+//Rutas Privadas
+//Crear un Middleware para que active la estrategia y autorize entrar a las rutas solo a los usuarios cuyo JWT lo permite
+const jwtMiddleware = passport.authenticate('jwt', {session:false});
+
+
+router.use('/mocion', jwtMiddleware, mocionRoutes);
+router.use('/alumnos', jwtMiddleware, alumnosRoutes); 
 //router.use('/donacion', donacionRoutes);
 //router.use('/donacion/canastas', canastasRoutes);
 
